aca3f68b609d7f406a4eefe7ed6f76741f486abd
Bas Nijholt <basnijholt@gmail.com>
2016-10-11 20:03:35 +0200

    Use find_packages(), closes #16

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='qstat-pretty',
@@ -7,7 +7,6 @@ setup(
     author_email='stefan@seemayer.de',
     license='GPL',
     url='https://github.com/sseemayer/qstat-pretty',
-
-    packages=['qstatpretty', 'qstatpretty.ttyutil'],
+    packages=find_packages(),
     scripts=['pstat']
 )
